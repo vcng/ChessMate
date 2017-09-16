95f101a5f66e1d5d1202eb517223f4cfe6cf19bc
Jeremy <jmpetrot@buffalo.edu>
2017-09-16 12:10:15 -0400

    Added basic logic for recognizing piece pick up and set down

--- pi/gameplay.py
+++ pi/gameplay.py
@@ -1,8 +1,41 @@
+from board import Board
+
+chess_board = Board()
+active_piece = None
+
+
 def toggle_piece(coord):
     """
     Event: A piece was lifted or set down at the specified coordinate
     :param coord: A 2-tuple representing the (row, col) coordinate on the board
     :return: None
     """
-    pass
+    global chess_board
+    global active_piece
 
+    row, col = coord[0], coord[1]
+    if row < 0 or row > 7 or col < 0 or col > 7:
+        raise Exception("Coordinate out of bounds (%s, %s)" % coord)
+
+    # If there was already a piece here, this must be a lift event
+    if chess_board[coord] is not None:
+        active_piece = chess_board.remove_piece(coord)
+    else:
+        # Otherwise, this must be a set down event
+        # Ensure that there is an active piece
+        if active_piece is not None:
+            # Set down the active piece at the given location
+            chess_board.set_piece(coord, active_piece)
+            active_piece = None
+        else:
+            raise Exception("Attempted to set down a piece before piece was selected")
+
+
+def start():
+    """
+    Initialize a new game
+    :return: None
+    """
+    global chess_board
+
+    chess_board.reset()
