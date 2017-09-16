7e5cd02cf1e84975f38912e3a1d83b5546e09266
Jeremy <jmpetrot@buffalo.edu>
2017-09-16 11:00:26 -0400

    Added toggle piece event handler interface

---
+++ pi/gameplay.py
@@ -0,0 +1,8 @@
+def toggle_piece(coord):
+    """
+    Event: A piece was lifted or set down at the specified coordinate
+    :param coord: A 2-tuple representing the (row, col) coordinate on the board
+    :return: None
+    """
+    pass
+
