85f5bd1cf5ae84f526d2f358f1672f0673cebe72
Mateusz Oramus <mateuszo@buffalo.edu>
2017-09-23 12:08:49 -0400

    Add files via upload

---
+++ Integrate.cpp
@@ -0,0 +1,52 @@
+// This is the Integrated Hardware Library 
+// for use with the ChessMate Project
+
+// Source File
+
+#include "Arduino.h"
+#include "Integrate.h"
+
+// Constructor definition
+Integrate::Integrate()
+{
+  return;
+}
+
+
+
+// XOR definition
+String Integrate::XOR(byte var1, byte var2)
+{
+  // var1 & var2 will be representing byte values
+  // ex) (byte var1 = 23) => 00010111
+  
+  byte xor_total; // xor'd byte value of var1 ^ var2
+  String final_byte; // final xor'd byte
+  
+  xor_total = var1 ^ var2; // XOR
+  final_byte = String(xor_total);
+
+
+  // OR //
+
+  /*
+
+  for(int i = 0; i <= 7; i++) {
+
+    final_byte = final_byte + bitRead(total,i);
+    // Read each bit of the byte based on position
+    // & then append to a final string
+  }
+
+  */
+  return final_byte;
+}
+
+
+// Coordinate definition
+String Integrate::Coordinate()
+{
+  return;
+}
+
+
---
+++ Integrate.h
@@ -0,0 +1,19 @@
+// This is the Integrated Hardware Library 
+// for use with the ChessMate Project
+
+// Header File
+#ifndef INTEGRATE_H
+#define INTEGRATE_H
+
+
+#include "Arduino.h"
+
+class Integrate
+{
+  public:
+    Integrate();
+    String XOR(byte, byte);
+    String Coordinate();
+};
+
+#endif
