5bdfec51a120d72e869e5f2f5aa2f3368a33b205
Jeremy <jmpetrot@buffalo.edu>
2017-09-23 17:21:51 -0400

    Added instructions and way to exit

--- pi/console.py
+++ pi/console.py
@@ -4,6 +4,20 @@ import util
 gameplay.start()
 moves = []
 
+print('''
+--- --- ---
+This is a simulation of the hardware, without actually needing the physical chess board.
+You will be asked to select the location of a piece to pick up, and where to set that
+piece down.
+
+Valid moves will be highlighted on the board, but these are NOT enforced. Feel free to
+move pieces around to whatever location you want and take a look at what their valid
+moves would be.
+
+To exit, enter -1.
+--- --- ---
+''')
+
 while True:
     print()
     print(gameplay.chess_board)
@@ -18,11 +32,17 @@ while True:
             print('Invalid value, try again')
             continue
 
+        if row == -1:
+            exit(0)
+
         try:
             col = int(input('Enter the column: '))
         except Exception:
             print('Invalid value, try again')
             continue
+
+        if col == -1:
+            exit(0)
 
         if not util.is_valid_location((row, col)) or gameplay.chess_board[(row, col)] is None:
             print('There is no piece at this location. Please try again.')
@@ -36,6 +56,7 @@ while True:
 
     print()
     print(gameplay.chess_board.get_string_representation(moves))
+    print('You are holding:', gameplay.active_piece)
 
     while True:
         try:
@@ -44,11 +65,17 @@ while True:
             print('Invalid value, try again')
             continue
 
+        if row == -1:
+            exit(0)
+
         try:
             col = int(input('Enter the column: '))
         except Exception:
             print('Invalid value, try again')
             continue
+
+        if col == -1:
+            exit(0)
 
         if not util.is_valid_location((row, col)) or gameplay.chess_board[(row, col)] is not None:
             print('There is a piece at this location, or the coordinate is out of bounds. Please try again.')
