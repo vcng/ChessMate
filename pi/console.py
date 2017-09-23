22d070b493aa20d367b88a9600776f680e691834
Jeremy <jmpetrot@buffalo.edu>
2017-09-23 17:29:29 -0400

    Added more comments describing what is going on at each section in the code

--- pi/console.py
+++ pi/console.py
@@ -4,6 +4,7 @@ import util
 gameplay.start()
 moves = []
 
+# Initial instructions
 print('''
 --- --- ---
 This is a simulation of the hardware, without actually needing the physical chess board.
@@ -18,14 +19,21 @@ To exit, enter -1.
 --- --- ---
 ''')
 
+# Simulation loop
 while True:
-    print()
-    print(gameplay.chess_board)
 
+    # Local variables
     row = 0
     col = 0
 
+    # Print the initial board
+    print()
+    print(gameplay.chess_board)
+
+    # Keep trying to pick up a piece until a valid instruction is given
     while True:
+
+        # Get the row
         try:
             row = int(input('Enter the row (-1 to exit): '))
         except Exception:
@@ -35,6 +43,7 @@ while True:
         if row == -1:
             exit(0)
 
+        # Get the column
         try:
             col = int(input('Enter the column (-1 to exit): '))
         except Exception:
@@ -44,21 +53,28 @@ while True:
         if col == -1:
             exit(0)
 
+        # Ensure valid location
         if not util.is_valid_location((row, col)) or gameplay.chess_board[(row, col)] is None:
             print('There is no piece at this location. Please try again.\n')
             continue
 
+        # Let gameplay know the piece was picked up
+        # It returns the highlighted tiles
         moves = gameplay.toggle_piece((row, col))[1]
 
         print('Possible legal moves:', moves)
 
         break
 
+    # Print out the board with the highlighted pieces
     print()
     print(gameplay.chess_board.get_string_representation(moves))
     print('You are holding:', gameplay.active_piece)
 
+    # Keep trying to put down a piece until a valid instruction is given
     while True:
+
+        # Get the row
         try:
             row = int(input('Enter the row (-1 to exit): '))
         except Exception:
@@ -68,6 +84,7 @@ while True:
         if row == -1:
             exit(0)
 
+        # Get the column
         try:
             col = int(input('Enter the column (-1 to exit): '))
         except Exception:
@@ -77,10 +94,12 @@ while True:
         if col == -1:
             exit(0)
 
+        # Ensure valid location
         if not util.is_valid_location((row, col)) or gameplay.chess_board[(row, col)] is not None:
             print('There is a piece at this location, or the coordinate is out of bounds. Please try again.\n')
             continue
 
+        # Let gameplay know the piece is being set down
         gameplay.toggle_piece((row, col))
 
         break
