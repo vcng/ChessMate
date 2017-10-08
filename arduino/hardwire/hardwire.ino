9b66169e5d9037e6e83609a28dc0099f89ed1eba
Jeremy <jmpetrot@buffalo.edu>
2017-10-08 13:54:46 -0400

    Now using full board, pin 43 should be grounded

--- arduino/hardwire/hardwire.ino
+++ arduino/hardwire/hardwire.ino
@@ -1,4 +1,4 @@
-#define SIZE 32
+#define SIZE 64
 #define LED_COUNT 64
 #define LED_PIN 52
 
@@ -6,10 +6,14 @@
 
 // Board pin mapping
 static const byte BOARD_PINS[] = {
-  22, 23, 24, 25, 26, 27, 28, 29,
-  30, 31, 32, 33, 34, 35, 36, 37,
-  38, 39, 40, 41, 42, 43, 44, 45,
-  46, 47, 48, 49, 50, 51, 52, 53
+  43, 43, 43, 43, 43, 43, 43, 43,
+  43, 43, 43, 43, 43, 43, 43, 43,
+  43, 43, 22, 23, 24, 25, 43, 43,
+  43, 43, 26, 27, 28, 29, 43, 43,
+  43, 43, 30, 31, 32, 33, 43, 43,
+  43, 43, 34, 35, 36, 37, 43, 43,
+  43, 43, 43, 43, 43, 43, 43, 43,
+  43, 43, 43, 43, 43, 43, 43, 43
 };
 
 byte board[SIZE];
@@ -98,6 +102,7 @@ void pollController() {
         int col = Serial.readStringUntil(':').toInt();
 
         // For debugging, echo the command to the pi for now
+        Serial.print("debug: ");
         Serial.print(cmd);
         Serial.print(" ");
         Serial.print(row);
