418e3cdaaf3b4a30ee9ee00ebc2c5f011112e6f6
Jeremy <jmpetrot@buffalo.edu>
2017-10-21 16:56:50 -0400

    Added debug to main

--- pi/main.py
+++ pi/main.py
@@ -2,12 +2,18 @@ from protocol import Protocol
 from gameplay import *
 
 if __name__ == "__main__":
-    protocol = Protocol()
+    protocol = Protocol(port='COM5', debug=True)
+    start()
 
     while True:
-        response = protocol.listen()
+        response = protocol.listen(debug=True)
         if response is 'error':
             exit('Error in protocol')
 
+        print 'GOT RESPONSE #1 "', response, '"'
+
         response = toggle_piece(response)
-        protocol.show_moves(response)
+
+        print 'GOT RESPONSE #2 "', response, '"'
+
+        protocol.show_moves(response, debug=True)
