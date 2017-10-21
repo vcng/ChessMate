8017273b4f66248688a050eca79dc6ae20fe8fd8
Andrew Balys <arbalys@buffalo.edu>
2017-10-21 12:37:47 -0400

    Updated arrays returned in to get each piece "get_moves" to allow for potential capturing of other teams pieces.

--- pi/board.py
+++ pi/board.py
@@ -1,11 +1,3 @@
-from pieces.king import King
-from pieces.queen import Queen
-from pieces.rook import Rook
-from pieces.knight import Knight
-from pieces.bishop import Bishop
-from pieces.pawn import Pawn
-
-
 class PieceType:
     """
     Enum representing all the different types of pieces
@@ -34,35 +26,6 @@ class Piece:
         self.is_black = is_black
         self.moved = False
 
-    def get_moves(self, coord, chess_board):
-        return {
-            PieceType.KING: King,
-            PieceType.QUEEN: Queen,
-            PieceType.ROOK: Rook,
-            PieceType.KNIGHT: Knight,
-            PieceType.BISHOP: Bishop,
-            PieceType.PAWN: Pawn
-        }[self.piece_type].get_moves(coord, self, chess_board)
-
-    def __str__(self):
-        """
-        String conversion override
-        :return: String representation of the piece
-        """
-        symbol = {
-            PieceType.KING: 'K',
-            PieceType.QUEEN: 'Q',
-            PieceType.ROOK: 'R',
-            PieceType.KNIGHT: 'H',
-            PieceType.BISHOP: 'B',
-            PieceType.PAWN: 'P'
-        }[self.piece_type] or '?'
-
-        if not self.is_black:
-            symbol = symbol.lower()
-
-        return symbol
-
 
 def create_starting_layout():
     """
@@ -72,28 +35,28 @@ def create_starting_layout():
     """
     return [
         [
-            Piece(PieceType.ROOK, True), Piece(PieceType.KNIGHT, True), Piece(PieceType.BISHOP, True),
-            Piece(PieceType.KING, True), Piece(PieceType.QUEEN, True), Piece(PieceType.BISHOP, True),
-            Piece(PieceType.KNIGHT, True), Piece(PieceType.ROOK, True)
+            Piece(PieceType.ROOK, False), Piece(PieceType.KNIGHT, False), Piece(PieceType.BISHOP, False),
+            Piece(PieceType.KING, False), Piece(PieceType.QUEEN, False), Piece(PieceType.BISHOP, False),
+            Piece(PieceType.KNIGHT, False), Piece(PieceType.ROOK, False)
         ],
-        [
-            Piece(PieceType.PAWN, True), Piece(PieceType.PAWN, True), Piece(PieceType.PAWN, True),
-            Piece(PieceType.PAWN, True), Piece(PieceType.PAWN, True), Piece(PieceType.PAWN, True),
-            Piece(PieceType.PAWN, True), Piece(PieceType.PAWN, True)
-        ],
-        [None] * 8,
-        [None] * 8,
-        [None] * 8,
-        [None] * 8,
         [
             Piece(PieceType.PAWN, False), Piece(PieceType.PAWN, False), Piece(PieceType.PAWN, False),
             Piece(PieceType.PAWN, False), Piece(PieceType.PAWN, False), Piece(PieceType.PAWN, False),
             Piece(PieceType.PAWN, False), Piece(PieceType.PAWN, False)
         ],
+        [None] * 8,
+        [None] * 8,
+        [None] * 8,
+        [None] * 8,
         [
-            Piece(PieceType.ROOK, False), Piece(PieceType.KNIGHT, False), Piece(PieceType.BISHOP, False),
-            Piece(PieceType.KING, False), Piece(PieceType.QUEEN, False), Piece(PieceType.BISHOP, False),
-            Piece(PieceType.KNIGHT, False), Piece(PieceType.ROOK, False)
+            Piece(PieceType.PAWN, True), Piece(PieceType.PAWN, True), Piece(PieceType.PAWN, True),
+            Piece(PieceType.PAWN, True), Piece(PieceType.PAWN, True), Piece(PieceType.PAWN, True),
+            Piece(PieceType.PAWN, True), Piece(PieceType.PAWN, True)
+        ],
+        [
+            Piece(PieceType.ROOK, True), Piece(PieceType.KNIGHT, True), Piece(PieceType.BISHOP, True),
+            Piece(PieceType.KING, True), Piece(PieceType.QUEEN, True), Piece(PieceType.BISHOP, True),
+            Piece(PieceType.KNIGHT, True), Piece(PieceType.ROOK, True)
         ],
     ]
 
@@ -104,6 +67,27 @@ class Board:
     Represents the state of an 8 x 8 game board, and allows the user to
     set pieces down and take pieces off
     """
+
+    def is_enemy(self, coord, piece):
+        """
+        :param coord: coord to check if enemy is there or not
+        :param piece: piece that would be taking the enemy
+        :return: return true if its an enemy else false
+        """
+
+        # row, col = coord[0], coord[1]
+
+        # if there is a piece at that location and piece color is other team (black or white) return true else false
+        return self.board[coord[0]][coord[1]] is not None and piece.is_black != self.board[coord[0]][coord[1]].is_black
+
+    @staticmethod
+    def is_valid_location(coord):
+        """
+        :param coord: coordinate to check to see if its a location on the board
+        :return: true:( on board ) or false:( off board )
+        """
+        row, col = coord[0], coord[1]
+        return 0 <= row <= 7 and 0 <= col <= 7
 
     def __init__(self):
         self.board = []
@@ -123,8 +107,6 @@ class Board:
         :param coord: A 2-tuple representing the (row, col) coordinate on the board
         :return: The removed piece
         """
-        assert 0 <= coord[0] <= 7 and 0 <= coord[1] <= 7
-        
         piece = self.board[coord[0]][coord[1]]
         self.board[coord[0]][coord[1]] = None
         return piece
@@ -137,8 +119,6 @@ class Board:
         :param piece: The piece to add
         :return: None
         """
-        assert 0 <= coord[0] <= 7 and 0 <= coord[1] <= 7
-
         if self.board[coord[0]][coord[1]] is not None:
             raise Exception("Attempted to set piece on coordinate %s, there already exists a piece" % coord)
 
@@ -150,8 +130,6 @@ class Board:
         :param coord: A 2-tuple representing the (row, col) coordinate on the board
         :return: The piece at the given coordinate if it exists, otherwise None
         """
-        assert 0 <= coord[0] <= 7 and 0 <= coord[1] <= 7
-
         return self.board[coord[0]][coord[1]]
 
     def __setitem__(self, coord, piece):
@@ -162,25 +140,4 @@ class Board:
         :param piece: The piece to add
         :return: None
         """
-        assert 0 <= coord[0] <= 7 and 0 <= coord[1] <= 7
-
         self.set_piece(coord, piece)
-
-    def __str__(self):
-        """
-        String conversion override
-        :return: String representation of the board
-        """
-        return self.get_string_representation([])
-
-    def get_string_representation(self, highlighted_coords):
-        output = "\t  0   1   2   3   4   5   6   7\n"
-        for i, row in enumerate(self.board):
-            output += str(i) + '\t|'
-            for j, piece in enumerate(row):
-                if (i, j) in highlighted_coords:
-                    output += '[' + str(piece or ' ') + ']|'
-                else:
-                    output += ' ' + str(piece or ' ') + ' |'
-            output += '\n'
-        return output
