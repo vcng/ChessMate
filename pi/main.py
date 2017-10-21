d1841defef5eba1b015dff944b7ee5bcca39fc6f
Jeremy <jmpetrot@buffalo.edu>
2017-10-21 12:09:09 -0400

    Added main driver

---
+++ pi/main.py
@@ -0,0 +1,13 @@
+from protocol import Protocol
+from gameplay import *
+
+if __name__ == "__main__":
+    protocol = Protocol()
+
+    while True:
+        response = protocol.listen()
+        if response is ['error']:
+            exit('Error in protocol')
+
+        response = toggle_piece(response)
+        protocol.show_moves(response)
