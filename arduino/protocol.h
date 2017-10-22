a092f76e86d919252b31cec7e95252e12d29e4cd
Miles <31706085+milesave@users.noreply.github.com>
2017-10-22 16:04:32 -0400

    Added comments

--- arduino/protocol.h
+++ arduino/protocol.h
@@ -4,8 +4,13 @@
 #include <Arduino.h>
 #include <string.h>
 
+//Initialize function for protocol
 void protocol_init();
+
+//Toggle command
 void protocol_toggle(int x, int y);
+
+//Listener for recieving input from serial
 void protocol_listen();
 
 #endif
